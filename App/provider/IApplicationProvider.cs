83e526bb0bdf12939528f07f9454c879088f8335
Oleg Shishkin <shishkin1966@gmail.com>
2022-06-24 17:09:18 +0300

    Update

--- App/provider/IApplicationProvider.cs
+++ App/provider/IApplicationProvider.cs
@@ -15,16 +15,40 @@ namespace WindowsFormsApp1.App
 
         void OnExit();
 
+        /**
+        * Ожидать без блокирования
+        * @param milliseconds - задержка ожидания
+        */
         void Wait(double milliseconds);
 
+        /**
+        * Ожидать с блокированием
+        * @param milliseconds - задержка ожидания
+        */
         void Sleep(int milliseconds);
 
+        /**
+        * Получить каталог программы
+        * @return - каталог программы
+        */
         string GetApplicationDir();
 
+        /**
+        * Получить полный путь приложения
+        * @return - путь
+        */
         string GetApplicationFullPath();
 
+        /**
+        * Получить имя приложения
+        * @return - имя приложения
+        */
         string GetApplicationName();
 
+        /**
+        * Получить имя файла приложения
+        * @return - имя файла приложения
+        */
         string GetApplicationFileName();
     }
 }
