aac057b5941e1d38773dfb137b56e43feb7f1dfd
Oleg Shishkin <shishkin1966@gmail.com>
2022-06-24 17:01:28 +0300

    Update

--- App/model/Main/MainModel.cs
+++ App/model/Main/MainModel.cs
@@ -1,12 +1,10 @@
 ﻿using ClearArchitecture.SL;
-using System.Threading;
 
 namespace WindowsFormsApp1.App
 {
     public class MainModel<T> : BaseModel<T> where T: MainForm
     {
         public const string NAME = "MainModel";
-        private SplashForm form;
 
         public MainModel(MainForm form) : base(NAME, (IModelView<T>)form)
         {
@@ -16,7 +14,7 @@ namespace WindowsFormsApp1.App
         {
             base.OnStart();
 
-            form = new SplashForm();
+            var form = new SplashForm();
             form.Show();
         }
     }
